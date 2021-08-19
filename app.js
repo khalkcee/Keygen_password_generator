b3d9388b27551a951baaf42118204ff8261e04c4
khalkcee <66587705+khalkcee@users.noreply.github.com>
2021-08-19 02:29:54 -0700

    Added files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const empty = "";
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*=-_";
+
+const pLength = document.getElementById("p-length");
+const upperCase = document.getElementById("p-uppercase");
+const lowerCase = document.getElementById("p-lowercase");
+const pNumber = document.getElementById("p-number");
+const pSymbol = document.getElementById("p-symbol");
+const submit = document.getElementById("submit");
+const password = document.getElementById("password");
+
+submit.addEventListener("click", () => {
+    let initialPassword = empty;
+    (upperCase.checked) ? initialPassword += uCase : "";
+    (lowerCase.checked) ? initialPassword += lCase : "";
+    (pNumber.checked) ? initialPassword += number : "";
+    (pSymbol.checked) ? initialPassword += symbol : "";
+
+    password.value = generatePassword(pLength.value, initialPassword)
+});
+
+function generatePassword(l, initialPassword) {
+    let pass = "";
+    for (let i = 0; i < l; i++) {
+        pass += initialPassword.charAt(Math.floor(Math.random() * initialPassword.length));
+    }
+    return pass;
+}
+
+// Copy password to clipboard
+
+const copy = document.getElementById("copy");
+
+copy.addEventListener("click", () => {
+    if (password.value == "") {
+        modal.style.display = "block";
+
+    }else {
+        password.select();
+        document.execCommand("copy");
+        alert("Password has been copied to clipboard");
+    }
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
